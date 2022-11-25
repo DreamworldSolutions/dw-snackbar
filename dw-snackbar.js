4f55a42438b7c6aaa15ffec7387142be0d03bc8c
VaidehiKheni4 <111484167+VaidehiKheni4@users.noreply.github.com>
2022-11-25 14:06:26 +0530

    feat: START update snackbar background-color (#30)
    feat: update snack bar background color

--- dw-snackbar.js
+++ dw-snackbar.js
@@ -1,5 +1,5 @@
-import { LitElement, html, css } from '@dreamworld/pwa-helpers/lit.js';
-import { repeat } from 'lit/directives/repeat.js';
+import { LitElement, html, css } from "@dreamworld/pwa-helpers/lit.js";
+import { repeat } from "lit/directives/repeat.js";
 import { layoutMixin } from "@dreamworld/pwa-helpers/layout-mixin.js";
 import { sortBy, debounce } from "lodash-es";
 
@@ -37,7 +37,7 @@ import "@dreamworld/dw-button";
  *   import {show as showSnackBar} from '@dreamworld/dw-snackbar/dw-snackbar.js';
  *
  *    showSnackBar({ message: ${message}), actionButton: {caption: ${buttonCaption}, callback} });
- * 
+ *
  * # TODO
  *  - When this component convert to lit(v2.0.0) use repeat directive instead of Array.map prototype to render multiple toast.
  */
@@ -88,7 +88,7 @@ export class DwSnackbar extends layoutMixin(LitElement) {
             var(--dw-snackbar-horizontal-margin, 24px);
           box-sizing: border-box;
           color: var(--dw-snackbar-text-color, var(--dw-on-surface-invert-color));
-          background-color: var(--dw-snackbar-background-color, var(--dw-surface-invert-color));
+          background-color: var(--dw-snackbar-background-color, var(--mdc-theme-on-surface));
           box-sizing: border-box;
           box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.14),
             0 1px 18px 0 rgba(0, 0, 0, 0.12);
